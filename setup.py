44415f44bda00837002a2f95fc02319900a2661c
Priscilla <priscilla.chk@gmail.com>
2020-03-06 18:08:33 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="", # Replace with your own username
+    name="topk_classification", 
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
